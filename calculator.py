7af30517e0c5c7323179931b503127ed592ac7de
Prajakta Kumbhar <prajaktakumbhar1425@gmail.com>
2026-07-24 23:05:25 +0530

    changed some lines

--- calculator.py
+++ calculator.py
@@ -125,5 +125,4 @@ x = (screen_width / 2) - (window_width / 2)
 y = (screen_height / 2) - (window_height / 2)
 window.geometry(f"{window_width}x{window_height}+{int(x)}+{int(y)}")
 
-
 window.mainloop()
